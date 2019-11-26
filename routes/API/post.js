89be9ff95226c657d77b81223bb0992bb008f883
Jake Garlick <jacobgarlick@msn.com>
2019-11-26 15:52:36 -0700

    added ability to get all posts, get post with attached id and delete post with attached id

--- routes/API/post.js
+++ routes/API/post.js
@@ -39,4 +39,66 @@ router.post("/", [auth, [
 
 });
 
+// Get Api/Post
+// Get all posts
+// private
+router.get("/", auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+});
+
+// Get Api/Post/:id
+// Get post by id
+// private
+router.get("/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: "Post Not Found" })
+        }
+
+        res.json(post)
+    } catch (err) {
+        console.error(err.message);
+        if (!err.kind === "ObjectId") {
+            return res.status(404).json({ msg: "Post Not Found" })
+        }
+        res.status(500).send("Server Error")
+    }
+});
+
+// Delete Api/Post/:id
+// Delete a post
+// private
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: "Post Not Found" })
+        }
+
+        // check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User is not authorized to delete"})
+        }
+
+        await post.remove();
+
+        res.json({ msg: "Post Removed" })
+    } catch (err) {
+        console.error(err.message);
+        if (!err.kind === "ObjectId") {
+            return res.status(404).json({ msg: "Post Not Found" })
+        }
+        res.status(500).send("Server Error")
+    }
+});
+
 module.exports = router;
