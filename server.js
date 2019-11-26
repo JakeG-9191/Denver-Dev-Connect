eb19cacecc9291f3d176db481d2e0be7128a1eea
Jake Garlick <jacobgarlick@msn.com>
2019-11-25 17:33:01 -0700

    added function for db connection from online DB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//DB Connection
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"))
 
