f3c7e663489a951d2893eac1c931d195ea1b0126
Jake Garlick <jacobgarlick@msn.com>
2019-11-29 15:17:27 -0700

    added ability to add and delete comments from user posts

--- routes/API/post.js
+++ routes/API/post.js
@@ -21,8 +21,8 @@ router.post("/", [auth, [
 
     try {
         const user = await User.findById(req.user.id).select("-password");
-    
-        const newPost = new Post ({
+
+        const newPost = new Post({
             text: req.body.text,
             name: user.name,
             avatar: user.avatar,
@@ -86,7 +86,7 @@ router.delete("/:id", auth, async (req, res) => {
 
         // check user
         if (post.user.toString() !== req.user.id) {
-            return res.status(401).json({ msg: "User is not authorized to delete"})
+            return res.status(401).json({ msg: "User is not authorized to delete" })
         }
 
         await post.remove();
@@ -151,6 +151,76 @@ router.put("/unlike/:id", auth, async (req, res) => {
         res.status(500).send("Server Error")
     }
 });
+
+// Post Api/Post/comment/:id
+// Comment on a post
+// private
+router.post("/comment/:id", [auth, [
+    check("text", "Text is required").not().isEmpty()
+],
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+
+});
+
+// Delete api/posts/comment/:id/:comment_id
+// Delete a comment on a post
+// private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // make sure comment exists
+        if (!comment) {
+            return res.status(404).json({ msg: "Comment does not exist" })
+        }
+
+        // make sure user deleting is actual user who made comment
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" })
+        }
+
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1)
+
+        await post.save();
+
+        res.json(post.comments);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+})
+
 
 
 module.exports = router;
