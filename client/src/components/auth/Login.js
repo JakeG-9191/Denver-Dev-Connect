57a829d57aaf13ea0c4ef81c54bf153dc176aba0
Jake Garlick <jacobgarlick@msn.com>
2019-12-02 11:51:43 -0700

    added redux store and reducers, imported redux to main app.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -14,7 +14,6 @@ const Login = () => {
     const onSubmit = async e => {
         e.preventDefault();
         console.log("Success");
-
     };
 
     return (
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
             <h1>
                 <Link to="/">
                     <i className="fas fa-code"></i> DevConnector
-            </Link>
+                </Link>
             </h1>
             <ul>
                 <li><Link to="!#">Developers</Link></li>
