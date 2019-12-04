65d9a8aeb2ec8cd364bd908e5cc5baeb0199d08d
Jake Garlick <jacobgarlick@msn.com>
2019-12-04 13:32:40 -0700

    began to work on displaying info for users that do and do not have profiles, added button for those that do not in order to link to page to create profile and submit to DB

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,17 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
+                <Link to="/dashboard">
+                    <i className="fas fa-user"></i>{" "}
+                    <span className="hide-sm">Dashboard
+                </span>
+                </Link>
+            </li>
+            <li>
                 <a onClick={logout} href="#!">
-                <i className="fas fa-sign-out-alt"></i>{" "}
-                <span className="hide-sm"></span>Logout
+                    <i className="fas fa-sign-out-alt"></i>{" "}
+                    <span className="hide-sm">logout
+                </span>
                 </a>
             </li>
         </ul>
@@ -31,7 +39,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
                     <i className="fas fa-code"></i> DevConnector
                 </Link>
             </h1>
-            { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
+            {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </nav>
     )
 };
