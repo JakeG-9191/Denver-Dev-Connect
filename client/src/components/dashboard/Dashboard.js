d7f6e73b92765c5a06a93b8ee56acc0c3e84e893
Jake Garlick <jacobgarlick@msn.com>
2019-12-05 10:23:58 -0700

    added functionality to display experience and education on dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
-import { getCurrentProfile } from "../../actions/profile";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
+import { getCurrentProfile } from "../../actions/profile";
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -20,6 +22,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         {profile !== null ? (
             <Fragment>
                 <DashboardActions />
+                <Experience experience={profile.experience} />
+                <Education education={profile.education} />
             </Fragment>
         ) : (
                 <Fragment>
