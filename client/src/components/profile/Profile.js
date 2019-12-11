5d912677053f1860c70f6400aa5a91d2561a6864
Jake Garlick <jacobgarlick@msn.com>
2019-12-11 11:41:34 -0700

    added github information to display on profile, including last 4 repos, stars, watchers and forks count along with description

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import PropTypes from 'prop-types';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -43,6 +44,9 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                             ))}
                         </Fragment>) : (<h4>No Education Credentials</h4>)}
                     </div>
+                    {profile.githubusername && (
+                        <ProfileGithub username={profile.githubusername}/>
+                    )}
                 </div>
             </Fragment>}
         </Fragment>
