8386f3babb3895c013494a82425bafa738a260de
Jake Garlick <jacobgarlick@msn.com>
2019-12-11 14:42:17 -0700

    made changes to prepare for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path")
 
 //DB Connection
 connectDB();
@@ -8,13 +9,21 @@ connectDB();
 // Init Middlewear
 app.use(express.json({ extended: false }))
 
-app.get("/", (req, res) => res.send("API Running"))
-
 // Define Routes
 app.use("/api/users", require("./routes/API/users"))
 app.use("/api/auth", require("./routes/API/auth"))
 app.use("/api/profile", require("./routes/API/profile"))
 app.use("/api/post", require("./routes/API/post"))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    })
+};
 
 const PORT = process.env.PORT || 3000;
 
