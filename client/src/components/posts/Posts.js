4246477bf16dcdf38b03b4866618a34beb465b13
Jake Garlick <jacobgarlick@msn.com>
2019-12-11 12:53:30 -0700

    added functionality to display posts for users, and added buttons to serve for like, dislike and comments in future as well as delete if user owns post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 import Spinner from "../layout/Spinner";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -9,10 +10,18 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         getPosts();
     }, [getPosts]);
 
-    return (
-        <div>
-            
-        </div>
+    return loading ? <Spinner /> : (
+        <Fragment>
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className=" fas fa-user"></i> Welcome To The Community
+            </p>
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </Fragment>
     )
 }
 
